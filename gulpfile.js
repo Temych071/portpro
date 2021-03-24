426922966d97f6afdcdf292f80d93ecfa43da7db
Goto.07 <goto.07@mail.ru>
2021-03-24 17:21:03 +0300

    start project

---
+++ gulpfile.js
@@ -0,0 +1,112 @@
+const { src, dest, watch, parallel, series } = require('gulp');
+
+const scss            = require('gulp-sass');
+const concat          = require('gulp-concat');
+const autoprefixer    = require('gulp-autoprefixer');
+const uglify          = require('gulp-uglify');
+const imagemin        = require('gulp-imagemin');
+const rename          = require('gulp-rename');
+const nunjucksRender  = require('gulp-nunjucks-render');
+const del             = require('del');
+const browserSync     = require('browser-sync').create();
+const moment          = require('moment');
+const momentTimezone  = require('moment-timezone');
+
+function browsersync() {
+  browserSync.init({
+    server:{
+      baseDir: 'app/'
+    },
+    notify:false
+  })
+}
+
+function nunjucks() {
+  return src('app/**/*.njk')
+    .pipe(nunjucksRender())
+    .pipe(dest('app'))
+    .pipe(browserSync.stream())
+}
+
+function styles() {
+  return src(
+    [
+      'app/modules/**/*.scss',
+      'app/scss/*.scss',
+      'app/fonts/**/*.scss',
+    ])
+    .pipe(scss({outputStyle: 'compressed'}))
+    .pipe(rename({
+      suffix : '.min'
+    }))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 10 versions'],
+      grid: true
+    }))
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream()) 
+}
+
+function scripts() {
+  return src([
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'node_modules/mixitup/dist/mixitup.js',
+    'node_modules/moment/moment.js',
+    'node_modules/moment-timezone/moment-timezone.js',
+    'app/js/main.js'
+  ])
+  .pipe(concat('main.min.js'))
+  .pipe(uglify())
+  .pipe(dest('app/js'))
+  .pipe(browserSync.stream())
+}
+
+function images() {
+  return src('app/images/**/*.*')
+  .pipe(imagemin([
+    imagemin.gifsicle({ interlaced: true }),
+    imagemin.mozjpeg({ quality: 75, progressive: true }),
+    imagemin.optipng({ optimizationLevel: 5 }),
+    imagemin.svgo({
+      plugins: [
+        { removeViewBox: true },
+        { cleanupIDs: false }
+      ]
+    })
+  ]))
+  .pipe(dest('dist/image'))
+}
+
+function build() {
+  return src([
+    'app/**/*.html',
+    'app/css/style.min.css',
+    'app/js/main.min.js'
+  ], {base: 'app' })
+  .pipe(dest('dist'))
+}
+
+function cleanDist() {
+  return del('dist')
+}
+
+function watching() {
+  watch(['app/scss/**/*.scss'], styles);
+  watch(['app/modules/**/*.scss'], styles);
+  watch(['app/**/*.njk'], nunjucks);
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+  watch(['app/**/*.html']).on('change', browserSync.reload);
+}
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.browsersync = browsersync;
+exports.watching = watching;
+exports.images = images;
+exports.nunjucks = nunjucks;
+exports.cleanDist = cleanDist;
+exports.build = series(cleanDist, images, build);
+
+exports.default = parallel(nunjucks, styles, scripts, browsersync, watching);
